caac17eff38483d0fda85239b14c1d46d8b838ee
Haohao QU <49360609+Quhaoh233@users.noreply.github.com>
2025-03-24 20:05:59 +0800

    Add files via upload

---
+++ code/main.py
@@ -0,0 +1,70 @@
+import os, sys
+import lightning as L
+import pytorch_lightning as pl
+from argparse import ArgumentParser
+from pytorch_lightning import Trainer
+import pytorch_lightning.callbacks as plc
+from pytorch_lightning.strategies import DDPStrategy
+from pytorch_lightning.loggers import TensorBoardLogger, CSVLogger
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+import parse
+import torch
+import pandas as pd
+from model_interface import MInterface
+from data_interface import MyDataModule
+from datasets import Dataset
+from torch.utils.data import DataLoader
+
+
+def load_callbacks(args):
+    callbacks = []
+    callbacks.append(plc.EarlyStopping(
+        monitor='metric',
+        mode='max',
+        patience=10,
+        min_delta=0.001
+    ))
+
+    callbacks.append(plc.ModelCheckpoint(
+        monitor='metric',
+        dirpath='../ckpt/' + args.data_name,
+        filename='{epoch:02d}-{metric:.3f}',
+        save_top_k=-1,
+        mode='max',
+        save_last=True,
+        #train_time_interval=args.val_check_interval
+        every_n_epochs=1
+    ))
+
+    if args.lr_scheduler:
+        callbacks.append(plc.LearningRateMonitor(
+            logging_interval='step'))
+    return callbacks
+
+
+def main(args):
+    print(f"Welcome to ChatEV! Now, we are working on Charging Data: {args.data_name}.")
+    L.seed_everything(args.seed)
+    
+    callbacks = load_callbacks(args)
+    model = MInterface(**vars(args))
+    if args.ckpt:
+        ckpt_path = '../ckpt/' + args.data_name + '/' + args.ckpt_name + '.ckpt'
+        ckpt = torch.load(ckpt_path, map_location='cpu')
+        model.load_state_dict(ckpt['state_dict'], strict=False)
+        print("load checkpoints from {}".format(ckpt_path))
+
+    data_module = MyDataModule(args)  # data_name=args.data_name, zone=args.zone, batch_size=args.batch_size
+    trainer = pl.Trainer(devices=[int(args.cuda)], accelerator='cuda', max_epochs=args.max_epochs, logger=True, callbacks=callbacks)  # single device
+    
+    if args.test_only:
+        pass
+    else:
+        trainer.fit(model=model, datamodule=data_module)  # train and valid
+    trainer.test(model=model, datamodule=data_module)  # test
+
+
+if __name__ == '__main__':
+    torch.multiprocessing.set_start_method('spawn')
+    args = parse.parse_args()
+    main(args)
